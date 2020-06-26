a34cd239112816dcbb07b4eafc37e821122fa7bc
Manfred Riem <mriem@manorrock.com>
2020-06-26 16:11:31 -0600

    Fixes issue #23 - Show a list of nodes

---
+++ src/main/java/com/manorrock/minskin/IndexController.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2002-2020 Manorrock.com. All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   1. Redistributions of source code must retain the above copyright notice, 
+ *      this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright notice,
+ *      this list of conditions and the following disclaimer in the documentation
+ *      and/or other materials provided with the distribution.
+ *   3. Neither the name of the copyright holder nor the names of its 
+ *      contributors may be used to endorse or promote products derived from this
+ *      software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.manorrock.minskin;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import org.omnifaces.oyena.action.ActionMapping;
+
+/**
+ * The controller for the /index page.
+ * 
+ * @author Manfred Riem (mriem@manorrock.com)
+ */
+@Named(value = "indexController")
+@RequestScoped
+public class IndexController {
+    
+    /**
+     * Get the nodes.
+     * 
+     * @return the nodes.
+     */
+    public List<String> getNodes() {
+        ArrayList<String> nodes = new ArrayList<>();
+        nodes.add("master");
+        return nodes;
+    }
+
+    /**
+     * Handle the /index request.
+     * 
+     * @return the index.xhtml page.
+     */
+    @ActionMapping(value = "/index")
+    public String index() {
+        return "/WEB-INF/ui/index.xhtml";
+    }
+}
